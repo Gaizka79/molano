aac1ebccddddce6960e1ef307a0ff904b32bd0b2
Gaizka79 <agaizka79@gmail.com>
2022-05-31 23:32:49 +0200

    hasita

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require("express");
+require ('body-parser');
+const cors = require('cors');
+const morgan = require ('./middlewares/morganConfig');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(morgan(':date[clf] :method :referrer :host :status :param[id] - :response-time ms :body'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', (req, res) => {
+    res.json({ message: "Kaixo nire web orrialde berritik!" });
+}); 
+
+app.get("/kaixo", (req, res) => {
+    res.json({ message: "Kaixo munduari!" });
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
